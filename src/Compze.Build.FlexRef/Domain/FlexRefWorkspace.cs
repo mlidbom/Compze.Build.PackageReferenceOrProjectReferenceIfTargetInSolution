d37e3d27c78c4a2535e2367904e7a54b472d7577
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-22 10:46:06 +0100

    refactoring

--- src/Compze.Build.FlexRef/Domain/FlexRefWorkspace.cs
+++ src/Compze.Build.FlexRef/Domain/FlexRefWorkspace.cs
@@ -55,7 +55,7 @@ class FlexRefWorkspace
 
       FlexRefPropsFile.Write(this);
       DirectoryBuildPropsFile.UpdateOrCreate();
-      new CSProjUpdater(this).UpdateAll();
+      CSProj.UpdateAll(this);
 
       foreach(var solution in SlnxSolution.FindAndParseAllSolutions(this))
          solution.UpdateNCrunchFile();
